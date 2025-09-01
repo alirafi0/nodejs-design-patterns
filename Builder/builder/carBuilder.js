2e796596cd89001d6ae0b98a5a22a680f4dce9b2
Ali Rafi <alirafie00700@gmail.com>
2025-09-01 04:44:51 -0700

    feat: add car builder

---
+++ Builder/builder/carBuilder.js
@@ -0,0 +1,33 @@
+export default class CarBuilderInterface {
+  setColor(color) {
+    throw new Error("setColor method must be implement.");
+  }
+  
+  setSeats() {
+    throw new Error("setSeats method must be implement.");
+  }
+
+  setEngine() {
+    throw new Error("setEngine method must be implement.");
+  }
+
+  setGps() {
+    throw new Error("setGps method must be implement.");
+  }
+
+  setSunroof() {
+    throw new Error("setSunroof method must be implement.");
+  }
+
+  setHeatedSeats() {
+    throw new Error("setHeatedSeats method must be implement.");
+  }
+
+  setParkingSensors() {
+    throw new Error("setParkingSensors method must be implement.");
+  }
+
+  setAbs() {
+    throw new Error("setAbs method must be implement.");
+  }
+}
